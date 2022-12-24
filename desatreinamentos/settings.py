fc8a83090afd0c2ce9df1c24ab9abadac4c821b5
desaengenharia <desa.softwares@gmail.com>
2022-12-24 11:01:20 -0300

    alterações de chaves

--- desatreinamentos/settings.py
+++ desatreinamentos/settings.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,13 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-szhibmngp-4zbo$is$jyabam=u7qh*us6p%qgmb75^cu8k=xt9'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
